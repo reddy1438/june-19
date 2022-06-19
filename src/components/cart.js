890c6a4081891d48fe4f172cf10d0a33b51e0b5f
govardhan reddy <reddygovardhan56@gmail.com>
2022-06-19 18:26:27 +0530

    jume19

---
+++ src/components/cart.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import "../styles/cart.css";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const Cart = ({ cart, setCart, handleChange }) => {
+  const [price, setPrice] = useState(0);
+
+  const handleRemove = (id) => {
+    const arr = cart.filter((item) => item.id !== id);
+    setCart(arr);
+    handlePrice();
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    cart.map((item) => (ans += item.amount * item.price));
+    setPrice(ans);
+  };
+
+  useEffect(() => {
+    handlePrice();
+  });
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <article>
+      {cart.map((item) => (
+        <div className="cart_box" key={item.id}>
+          <div className="cart_img">
+            <img src={item.img} alt="" />
+            <p>{item.title}</p>
+          </div>
+          <div>
+            <button onClick={() => handleChange(item, 1)}>+</button>
+            <button>{item.amount}</button>
+            <button onClick={() => handleChange(item, -1)}>-</button>
+          </div>
+          <div>
+            <span>{item.price}</span>
+            <button onClick={() => handleRemove(item.id)}>Remove</button>
+          </div>
+        </div>
+      ))}
+      <div className="total">
+        <span>Total Price of your Cart</span>
+        <span>Rs : {price}</span>
+      </div>
+      <div className="alert">
+      <Button variant="outlined" onClick={handleClickOpen}>
+        This App Using Location
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    </article>
+  );
+};
+
+export default Cart;
